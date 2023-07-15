bca2a1a82201d9dd886529dc49b4fab2d3dee02c
Pedro Furlan <pedrofurlan6@gmail.com>
2023-07-15 18:46:11 -0300

    Creating useWIndowsSize hook and start the Login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,10 +1,29 @@
+"use client";
 import { LoginHeader } from "@/components/LoginHeader";
+import Lottie from "lottie-react";
+import moneyAnimation from "@/lib/lottie/money.json";
+import useWindowSize from "@/hooks/useWindowsSize";
 
 export default function Login() {
+  const { isMobile } = useWindowSize()
+
   return (
-    <div className="h-screen w-full bg-gray-900">
+    <div className="h-screen w-full bg-gray-950 overflow-y-auto">
       <LoginHeader />
-      <h1>ola</h1>
+      <div className="h-max flex flex-col items-center justify-center gap-24 md:flex-row">
+        <Lottie
+          animationData={moneyAnimation}
+          loop={true}
+          style={{ width: isMobile ? 100 : 300, height: isMobile ? 100 : 300 }}
+        />
+        <div className="flex flex-col gap-4 items-center justify-center">
+          <input
+            className="border border-gray-300 px-4 py-2 rounded-md focus:outline-none focus:ring-2 text-gray-700 focus:ring-amber-500"
+            type="text"
+            placeholder="Email"
+          />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
---
+++ src/hooks/useWindowsSize.ts
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "react";
+
+const useWindowSize = () => {
+  const [windowSize, setWindowSize] = useState<{ width: number; isMobile: boolean }>({
+    width: 0,
+    isMobile: false,
+  });
+
+  useEffect(() => {
+    const handleResize = () => {
+      const screenWidth = window.innerWidth;
+      const isMobile = screenWidth < 768;
+
+      setWindowSize({ width: screenWidth, isMobile: isMobile });
+    };
+
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return windowSize;
+};
+
+export default useWindowSize;
